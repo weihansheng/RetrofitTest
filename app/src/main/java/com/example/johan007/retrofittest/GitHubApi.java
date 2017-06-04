79786ad601dec7eefffd64e666eec37fa1a5cb53
weihansheng <johan007@126.com>
2017-06-04 17:51:29 +0800

    public

--- app/src/main/java/com/example/johan007/retrofittest/GitHubApi.java
+++ app/src/main/java/com/example/johan007/retrofittest/GitHubApi.java
@@ -11,8 +11,6 @@ import retrofit2.http.Path;
  */
 
 public interface GitHubApi {
-  /*  @GET("repos/{owner}/{repo}/contributors")
-    Call<ResponseBody> contributorsBySimpleGetCall(@Path("owner") String owner, @Path("repo")String repo);*/
     @GET("repos/square/retrofit/contributors")
     Call<List<ResponseBody>> contributorsBySimpleGetCall();
 }
--- app/src/main/java/com/example/johan007/retrofittest/MainActivity.java
+++ app/src/main/java/com/example/johan007/retrofittest/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.johan007.retrofittest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 
@@ -20,30 +22,34 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn= (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getData();
+            }
+        });
+
+    }
+    public void getData(){
         Retrofit retrofit = new Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
                 .baseUrl("https://api.github.com/")
                 .build();
-        GitHubApi repo = retrofit.create(GitHubApi.class);
 
+        GitHubApi repo = retrofit.create(GitHubApi.class);
         Call<List<ResponseBody>> call = repo.contributorsBySimpleGetCall();
         call.enqueue(new Callback<List<ResponseBody>>() {
             @Override
             public void onResponse(Call<List<ResponseBody>> call, Response<List<ResponseBody>> response) {
-                Toast.makeText(MainActivity.this,"onResponse",Toast.LENGTH_SHORT).show();
-                Log.i("onResponse", response.toString());
-
-                    /*Gson gson = new Gson();
-                    ArrayList<ResponseBody> contributorsList = gson.fromJson(response.toString(), new TypeToken<List<ResponseBody>>(){}.getType());
-                    for (ResponseBody contributor : contributorsList){
-                        Log.d("login",contributor.getLogin());
-                        Log.d("contributions",contributor.getContributions()+"");
-                    }*/
+                Toast.makeText(MainActivity.this,"请求成功",Toast.LENGTH_SHORT).show();
+                Log.i("onResponse", "共获取contributors数："+response.body().size());
 
             }
 
